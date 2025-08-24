63c685dda85a8bd8c451ff668026585e53103aee
darkboss1 <74151069+darkboss1@users.noreply.github.com>
2025-08-24 20:36:20 +0600

    darkboss1-website-scrapper-boss

---
+++ install/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install required dependencies
+
+pip install requests
+pip install beautifulsoup4
+pip install termcolor
+pip install selenium
+pip install webdriver-manager
+pip install stem
+pip install tbselenium stem
+pip install pyvirtualdisplay
+
+sudo apt-get update && apt-get install -y firefox
+sudo apt-get install -y tor
+
+# Make the script executable
+chmod +x ../Uscrapper-vanta.py
+
+echo "Installation complete."
